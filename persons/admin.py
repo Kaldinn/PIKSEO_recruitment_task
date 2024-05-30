2b52f4cab87278708d8a669ba80818d659c6d43d
Kaldinn <danielskulimowski.biuro@gmail.com>
2024-05-30 11:18:52 +0200

    Third Task
    - Create a superuser for admin pannel,
    - Register all models,
    - Add list filters to improve navigation

--- persons/admin.py
+++ persons/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Persons, Skills, Position
 
-# Register your models here.
+class PersonsAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'age', 'position')
+    search_fields = ('first_name', 'last_name', 'skills__name', 'position__name')
+    list_filter = ('age', 'position', 'skills')
+    filter_horizontal = ('skills',)
+
+admin.site.register(Persons, PersonsAdmin)
+admin.site.register(Skills)
+admin.site.register(Position)
--- persons/models.py
+++ persons/models.py
@@ -4,10 +4,15 @@ from django.db import models
 class Skills(models.Model):
     name = models.CharField(verbose_name="Imię", max_length=64, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Position(models.Model):
     name = models.CharField(verbose_name="Stanowisko", max_length=64, unique=True)
 
+    def __str__(self):
+        return self.name
 
 class Persons(models.Model):
     first_name = models.CharField(verbose_name="Imię", max_length=64)
@@ -17,3 +22,6 @@ class Persons(models.Model):
     position = models.ForeignKey(
         Position, verbose_name="Stanowisko", null=True, on_delete=models.SET_NULL
     )
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
